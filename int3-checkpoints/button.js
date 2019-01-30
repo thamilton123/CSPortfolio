11d1eac0db19cc97dec4ab0b4b1e30d06a7b196d
Great and Powerful Travo <thamilton.sh@mysummitps.org>
2019-01-30 21:57:49 +0000

    rick

--- int3-checkpoints/button.js
+++ int3-checkpoints/button.js
@@ -1,5 +1,12 @@
-var button1 = document.getElementById("button1");
-button1.style.position = "absolute";
-button1.addEventListener("mousemove", function(){
-    
-})
+var vid = document.getElementById("vid");
+vid.style.position = "absolute";
+
+function activateYikes() {
+    vid.style.top = Math.random() * 500 + "px";
+    vid.style.left = Math.random() * 1000 + "px";
+}
+
+        vid.addEventListener("mousemove", function() {
+            activateYikes();
+        });
+
